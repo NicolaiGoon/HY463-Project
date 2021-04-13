32c4d25347fc7c78c1d4fd11c4b554453fb6db95
Nikolaos Gounakis 3932 <csd3932@csd.uoc.gr>
2021-04-13 14:54:53 +0300

    Add script to read contents of NXML

---
+++ readxml.py
@@ -0,0 +1,101 @@
+import xml.etree.ElementTree as ET
+
+
+def readFileXML(file):
+    """
+    Opens an nxml file , reads XML content and returns and object representing that file
+
+    Parameters
+    ----------
+        file: the file path to read
+    """
+
+    with open(file, 'r', encoding='utf-8') as xml_file:
+        xml_tree = ET.parse(xml_file)
+        root = xml_tree.getroot()
+
+        # find id
+        ids = root.findall('./front/article-meta/article-id')
+        for id in ids:
+            if id.attrib["pub-id-type"] == 'pmc':
+                pmc_id = id.text
+        # print("Pmcid: "+pmc_id)
+
+        # find title
+        title = root.find('.//article-title')
+        title = title.text
+        # print("Title: "+title)
+
+        # find abstract
+        abstr = root.find('.//abstract')
+        abstract = getDescendantsText(abstr)
+        # print("Abstract: "+abstract)
+
+        # find body
+        body = root.find('.//body')
+        body = getDescendantsText(body)
+        # print('Body: '+body)
+
+        # find journal
+        journal = root.find('.//journal-title')
+        journal = getDescendantsText(journal)
+        # print('journal: '+journal)
+
+        # find publisher
+        publisher = root.find('.//publisher-name')
+        publisher = getDescendantsText(publisher)
+        # print('publisher: '+publisher)
+
+        # find authors
+        authors = root.find('.//contrib-group')
+        authors_list = []
+        for author in authors:
+            if author.attrib['contrib-type'] == 'author':
+                author_item = ''
+                for elem in author.iter():
+                    if elem.tag == 'name':
+                        # get name and surname
+                        author_item += elem[0].text.strip() + \
+                            ' ' + elem[1].text.strip()
+                authors_list.append(author_item)
+        # print("authors: "+str(authors_list))
+
+        # find categories
+        categories = root.find('.//article-categories')
+        categories_list = []
+        for category in categories:
+            categories_list.append(getDescendantsText(category).strip())
+        # print('categories: '+str(categories_list))
+
+        document = {
+            "pmc_id": pmc_id,
+            "title": title,
+            "abstract": abstract,
+            "body": body,
+            "journal": journal,
+            "publisher": publisher,
+            "authors": authors_list,
+            "categories": categories_list
+        }
+        print(document)
+        return document
+
+
+def getDescendantsText(node):
+    '''
+    returns all node descendants text area
+
+    Parameters:
+    ----------
+        node: node of xml tree
+    '''
+    res = ''
+    try:
+        for d in node.itertext():
+            res += d.strip()
+    except:
+        res = ''
+    return res
+
+
+readFileXML("C:\\Users\\xgoun\\Desktop\\PROGRAMS\\HY463\\project\\HY463-Project\\Data\\MiniCollection\\diagnosis\\Topic_1\\0\\1852545.nxml")
