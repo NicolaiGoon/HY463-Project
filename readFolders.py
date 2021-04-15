f3b7de6db28e18c523dbba4eb75ad08939d3ec21
Nikolaos Gounakis 3932 <csd3932@csd.uoc.gr>
2021-04-15 13:56:52 +0300

    call function to analyze each doc

--- readFolders.py
+++ readFolders.py
@@ -1,14 +1,16 @@
 import os
+import readxml
 
 
-def readAllFiles(folder, callback):
+def readAllFiles(folder, callback, *extra):
     """
     Reads all nxml files in a folder recusrively and calls the callback function
     """
     for root, dirs, files in os.walk(folder):
         for file in files:
+            path = os.path.join(root, file)
             # skip files that are not nxml
-            if os.path.join(root, file)[-5:] != '.nxml':
+            if path[-5:] != '.nxml':
                 continue
-            print(os.path.join(root, file))
-            callback(os.path.join(root, file))
+            doc = readxml.readFileXML(path)
+            callback(doc, path, extra[0])
