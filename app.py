a1e1ac68f61fecb58aecce7b016d2fc74de21131
Nikolaos Gounakis 3932 <csd3932@csd.uoc.gr>
2021-04-19 21:20:30 +0300

    refactory

--- app.py
+++ app.py
@@ -8,19 +8,27 @@ import readxml
 import analyzer
 import indexer
 import time
+from Document import Document
 
-doc = readxml.readFileXML(
-    "C:\\Users\\xgoun\\Desktop\\PROGRAMS\\HY463\\project\\HY463-Project\\Data\\MiniCollection\\diagnosis\\Topic_1\\0\\1852545.nxml")
+# doc = readxml.readFileXML(
+#     "C:\\Users\\xgoun\\Desktop\\PROGRAMS\\HY463\\project\\HY463-Project\\Data\\MiniCollection\\diagnosis\\Topic_1\\0\\1852545.nxml")
 
-s = {
-    'authors': ['Νίκος Γουνάκης', 'Αντώνης Γουνάκης'],
-    'body': 'The???!@#$$%^&*()_ ? και ο γουνάκης η το Ελλάδα www.oof.com post-procedural παιζει course was uneventful; takotsubo cardiomyopathy was the final diagnosis and the patient was, thus, discharged with a therapy consisting of aspirin, diltiazem, ramipril and atorvastatin.Figure 1Twelve-lead electrocardiogram on admission.'
-}
 
-f = {
-    'body': 'ο Νίκος Γουνάκης σπουδάζει στο τμήμα επιστήμης υπολογιστών , όμως και ο Αντώνης σπουδάζει εκεί. ο Νίκος παίζει κιθάρα',
-    'authors': ['Νίκος Γουνάκης']
-}
+def testIndexing():
+    s = {
+        'authors': ['Νίκος Γουνάκης', 'Αντώνης Γουνάκης'],
+        'body': 'The???!@#$$%^&*()_ ? και ο γουνάκης η το Ελλάδα www.oof.com post-procedural παιζει course was uneventful; takotsubo cardiomyopathy was the final diagnosis and the patient was, thus, discharged with a therapy consisting of aspirin, diltiazem, ramipril and atorvastatin.Figure 1Twelve-lead electrocardiogram on admission.'
+    }
+
+    f = {
+        'body': 'ο Νίκος Γουνάκης σπουδάζει στο τμήμα επιστήμης υπολογιστών , όμως και ο Αντώνης σπουδάζει εκεί. ο Νίκος παίζει κιθάρα',
+        'authors': ['Νίκος Γουνάκης']
+    }
+    f = Document('f', 'path to f', f)
+    s = Document('s', 'path to s', s)
+    terms = analyzer.analyzeTerms([f, s])
+    sorted_terms = sorted(terms.keys())
+    indexer.index([f, s], terms, sorted_terms)
 
 
 def startIndexing():
@@ -37,4 +45,6 @@ def startIndexing():
     print("Total docs: "+str(len(docs))+"\nTotal terms: " +
           str(len(terms))+"\nTotal Time: "+str(time.time()-start))
 
+
 startIndexing()
+#testIndexing()
