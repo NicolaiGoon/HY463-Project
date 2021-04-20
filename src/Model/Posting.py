386ddf029868da37968771d9fc7cc5c3314f1efc
Nikolaos Gounakis 3932 <csd3932@csd.uoc.gr>
2021-04-20 12:30:30 +0300

    Add class to read form PostingFile.txt

---
+++ src/Model/Posting.py
@@ -0,0 +1,23 @@
+from src.Model import readIndex
+import ast
+
+
+class Posting:
+    """
+    represents a posting entry from PostingFile.txt
+    """
+
+    def __init__(self, line_pointer):
+        tokens = readIndex.getIndexLineToknized('Posting', line_pointer)
+        if tokens == '':
+            raise Exception('Invalid line pointer: '+line_pointer)
+        self.doc_id = int(tokens[0])
+        self.tf = float(tokens[1])
+        self.appearances = ast.literal_eval(tokens[2])
+        self.DocumentPointer = int(tokens[3])
+
+    def display(self):
+        print('---------- Posting Entry --------')
+        print('Doc id: '+str(self.doc_id)+'\nTF: '+str(self.tf)+'\nAppearances: ' +
+              str(self.appearances)+'\nDocument Pointer: '+str(self.DocumentPointer))
+        print('---------------------------------')
