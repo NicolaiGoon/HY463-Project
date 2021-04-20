7e580e339af71a88de0c35f8e1d3755e3c5a047c
Nikolaos Gounakis 3932 <csd3932@csd.uoc.gr>
2021-04-20 12:42:46 +0300

    Add class to read from DocumentsFile.txt

---
+++ src/Model/DocumentFileEntry.py
@@ -0,0 +1,21 @@
+from src.Model import readIndex
+
+
+class DocumentFileEntry:
+    """
+    class representing an entry to DocumentsFile.txt
+    """
+
+    def __init__(self, line_pointer):
+        tokens = readIndex.getIndexLineToknized('Documents', line_pointer)
+        if tokens == '':
+            raise Exception('Invalid line pointer: '+line_pointer)
+        self.id = int(tokens[0])
+        self.path = tokens[1]
+        self.norm = float(tokens[2])
+
+    def display(self):
+        print('----- DocumentFile Entry --------')
+        print('ID: '+str(self.id)+'\nPath: ' +
+              self.path+'\nNorm: '+str(self.norm))
+        print('---------------------------------')
