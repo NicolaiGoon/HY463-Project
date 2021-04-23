0ba8637294b4fa0f1ef63487e99973fc8158742a
Nikolaos Gounakis 3932 <csd3932@csd.uoc.gr>
2021-04-23 19:35:24 +0300

    Add multiple word query evaluation
    + document scoring

--- src/Model/search.py
+++ src/Model/search.py
@@ -2,54 +2,62 @@ from src.Model.Vocabulary import Vocabulary
 from src.Model.Posting import Posting
 from src.Model.DocumentFileEntry import DocumentFileEntry
 from src.Model.Document import Document
+from src.Model import Utilities
+import operator
+
 
 def search(query):
+    '''
+    returns the docs that match the query words
+    '''
     # load vocabulary
     vocab = Vocabulary().entries
+
     # query is represented as a document
-    query = Document(0,'Query',{'body' : query})
-    docs = {}
-    # find documents for every word
+    query = Document(0, 'Query', {'body': query})
+    
+    # for every term , contains the posts
+    posts = {}
+
+    # holds the docs returned
+    results = {}
+
+    # find documents for every word in query
     for word in query.content:
         try:
             term = vocab[word]
         except:
+            print(word)
             continue
-        docs = getDocsOfTerm(docs,term)
+        posts = getPosts(term, posts)
     
-    return docs
+    for doc_id in posts:
+        # take the doc pointer from the first term post
+        doc_pointer = posts[doc_id][next(iter(posts[doc_id]))].DocumentPointer
+        doc = DocumentFileEntry(doc_pointer)
+        # add score to doc as attribute
+        Utilities.CosSim(doc, query, vocab, posts)
+        results[doc_id] = doc
 
-# def getPosts(pointer,vocab,query):
-#     posts = []
-#     n = next_key(vocab,query)
-#     end_pointer = vocab[n].PostingPointer
-#     while True:
-#         if pointer == end_pointer - 1: 
-#             break
-#         post = Posting(pointer)
-#         posts.append(post)
-#         pointer += 1
-#     return posts
+    return sorted(results.values(), key=operator.attrgetter('score'))
 
-def getPosts(pointer):
-    posts = []
+
+def getScore(doc, query, vocab, posts):
+    '''
+    add scores to the document
+    '''
+    print(Utilities.CosSim(doc, query, vocab, posts))
+    return doc
+
+
+def getPosts(term, posts={}):
+    pointer = term.PostingPointer
     while True:
         post = Posting(pointer)
-        if post.stop: 
+        if post.stop:
             break
-        posts.append(post)
+        if post.doc_id not in posts:
+            posts[post.doc_id] = {}
+        posts[post.doc_id][term.id] = post
         pointer += 1
     return posts
-
-def getDocsOfTerm(docs,term):
-    posts = getPosts(term.PostingPointer)
-    for post in posts:
-        doc = DocumentFileEntry(post.DocumentPointer)
-        docs[doc.id] = doc
-    return docs
-
-# see https://stackoverflow.com/questions/61058709/return-next-key-of-a-given-dictionary-key-python-3-6
-def next_key(dict, key):
-    keys = iter(dict)
-    key in keys
-    return next(keys, False)
