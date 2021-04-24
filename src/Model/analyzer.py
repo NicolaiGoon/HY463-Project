3ae3ee3534935a88ef65235387f1f275ff7b3d26
Nikolaos Gounakis 3932 <csd3932@csd.uoc.gr>
2021-04-24 09:50:45 +0300

    Add progress bars

--- src/Model/analyzer.py
+++ src/Model/analyzer.py
@@ -3,7 +3,7 @@ from src.Model import readxml
 from src.Model.Term import Term
 import collections
 import os
-
+from alive_progress import alive_bar
 
 def analyzeAllDocs(folder):
     """
@@ -12,32 +12,36 @@ def analyzeAllDocs(folder):
     docs = {}
 
     # read all files in folder
-    for root, dirs, files in os.walk(folder):
-        for file in files:
-            path = os.path.join(root, file)
-            # skip files that are not nxml
-            if path[-5:] != '.nxml':
-                continue
-            doc = readxml.readFileXML(path)
-            docs[doc.id] = doc
+    with alive_bar(title='Reading Documents:', bar='classic') as bar:
+        for root, dirs, files in os.walk(folder):
+            for file in files:
+                path = os.path.join(root, file)
+                # skip files that are not nxml
+                if path[-5:] != '.nxml':
+                    continue
+                doc = readxml.readFileXML(path)
+                docs[doc.id] = doc
+                bar()
 
     return docs
 
 
 def analyzeTerms(docs):
     terms = {}
-    for doc in docs:
-        # keep words that df is rised
-        set = {}
-        for word in docs[doc].content:
-            # find if term exists
-            if word in terms and word not in set:
-                terms[word].setDf(terms[word].df + 1)
-            else:
-                new_term = Term(word)
-                new_term.setDf(1)
-                terms[word] = new_term
-            terms[word].addAppearance(docs[doc], word)
+    with alive_bar(title='Reading Terms:', bar='classic') as bar:
+        for doc in docs:
+            # keep words that df is rised
+            set = {}
+            for word in docs[doc].content:
+                # find if term exists
+                if word in terms and word not in set:
+                    terms[word].setDf(terms[word].df + 1)
+                else:
+                    new_term = Term(word)
+                    new_term.setDf(1)
+                    terms[word] = new_term
+                terms[word].addAppearance(docs[doc], word)
+                bar()
 
     return terms
 
