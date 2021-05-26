88300f75d0ee43c460385c3aea68a0db8231c3b2
Nikolaos Gounakis 3932 <csd3932@csd.uoc.gr>
2021-05-26 16:58:16 +0300

    Add topic number

--- src/Model/Topic.py
+++ src/Model/Topic.py
@@ -2,13 +2,15 @@ class Topic:
     '''
     Represetns a topic
     '''
-    def __init__(self,type,description,summary):
+    def __init__(self,type,description,summary,number):
         self.type = type
         self.description = description
         self.summary = summary
+        self.number = number
 
     def display(self):
         print('------ Topic ------')
+        print("NUmber: "+self.number)
         print("Type: "+self.type)
         print("Description: "+self.description)
         print("Summary: "+self.summary)
